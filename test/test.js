257d37ff10d8ac8894d0c32c69ce30ac19a06dc5
Maira Bello <maira.araujo@liferay.com>
2015-06-05 10:27:57 -0300

    Uses gulp.src instead of overcomplicated functions that basically just reimplemented it

--- test/test.js
+++ test/test.js
@@ -1,81 +1,57 @@
 var assert = require('assert');
 var babelGlobals = require('../index');
 var fs = require('fs');
+var gulp = require('gulp');
 var gutil = require('gulp-util');
 var path = require('path');
 
 module.exports = {
 	testBuildGlobals: function(test) {
-		buildGlobals([loadStreamFile('main.js')], null, function(bundle) {
-			assert.ok(bundle);
-			assert.strictEqual('bundle.js', bundle.path);
+		gulp.src('test/assets/main.js')
+			.pipe(babelGlobals())
+			.on('data', function(bundle) {
+				assert.ok(bundle);
+				assert.strictEqual('bundle.js', bundle.path);
 
-			eval(bundle.contents.toString());
-			assert.ok(this.myGlobals);
-			assert.ok(this.myGlobals.main);
-			assert.strictEqual('foo bar', this.myGlobals.main);
+				eval(bundle.contents.toString());
+				assert.ok(this.myGlobals);
+				assert.ok(this.myGlobals.main);
+				assert.strictEqual('foo bar', this.myGlobals.main);
 
-			assert.ok(bundle.sourceMap);
-			assert.strictEqual('bundle.js', bundle.sourceMap.file);
-			test.done();
-		});
+				assert.ok(bundle.sourceMap);
+				assert.strictEqual('bundle.js', bundle.sourceMap.file);
+			})
+			.on('end', function() {
+				test.done();
+			});
 	},
 
 	testBuildGlobalsBundleFileName: function(test) {
-		buildGlobals([loadStreamFile('main.js')], {bundleFileName: 'foo.js'}, function(bundle) {
-			assert.ok(bundle);
-			assert.strictEqual('foo.js', bundle.path);
+		gulp.src('test/assets/main.js')
+			.pipe(babelGlobals({bundleFileName: 'foo.js'}))
+			.on('data', function(bundle) {
+				assert.ok(bundle);
+				assert.strictEqual('foo.js', bundle.path);
 
-			eval(bundle.contents.toString());
-			assert.ok(this.myGlobals);
-			assert.ok(this.myGlobals.main);
-			assert.strictEqual('foo bar', this.myGlobals.main);
+				eval(bundle.contents.toString());
+				assert.ok(this.myGlobals);
+				assert.ok(this.myGlobals.main);
+				assert.strictEqual('foo bar', this.myGlobals.main);
 
-			assert.ok(bundle.sourceMap);
-			assert.strictEqual('foo.js', bundle.sourceMap.file);
-			test.done();
-		});
+				assert.ok(bundle.sourceMap);
+				assert.strictEqual('foo.js', bundle.sourceMap.file);
+			})
+			.on('end', function() {
+				test.done();
+			});
 	},
 
 	testErrorInvalidJs: function(test) {
-		buildGlobals(
-			[loadStreamFile('invalid.js')],
-			null,
-			function() {},
-			function(error) {
+		gulp.src('test/assets/invalid.js')
+			.pipe(babelGlobals())
+			.on('error', function(error) {
 				assert.ok(error);
-				test.done();
-			}
-		);
+				test.done()
+			});
 	}
 };
-
-
-function loadStreamFile(filePath) {
-	var basePath = path.join(__dirname, 'assets');
-	filePath = path.resolve(basePath, filePath);
-    return new gutil.File({
-        cwd: __dirname,
-        base: basePath,
-        path: filePath,
-        contents: fs.readFileSync(filePath)
-    });
-}
-
-function buildGlobals(sources, options, callback, errorCallback) {
-    var stream = babelGlobals(options);
-    var bundle;
-    stream.on('data', function(file) {
-    	bundle = file;
-    });
-    stream.on('end', function() {
-    	callback(bundle);
-    });
-    stream.on('error', function(error) {
-    	errorCallback && errorCallback(error);
-    });
-    sources.forEach(function(source) {
-        stream.write(source);
-    });
-    stream.end();
-}
